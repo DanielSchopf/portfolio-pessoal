458448c22bc3a01827b2b45b826da112b47c6ce3
Daniel Schopf <danielschopf04@gmail.com>
2024-09-05 01:52:56 -0300

    Responsividade adicionada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-scroll';
 import './styles/navbar.css';
 
 function Navbar() {
+
+    const[isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <nav className='navbar'>
-            <div className='navbar-links'>
+            <button className='menu-toggle' onClick={toggleMenu}>
+                &#9776;
+            </button>
+            <div className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
                 <Link to='sobre' className='navbar-link' smooth={true} duration={500}>Sobre mim</Link>
                 <Link to='formacao' className='navbar-link' smooth={true} duration={500}>Formação</Link>
             </div>
